319c745119b32b03e4925f16f13eccdb14827e51
blxerdout <blxerdout@gmail.com>
2023-04-07 07:40:37 -0500

    Fixed js syntax

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'));
