662be795d3f365c2922e7aa946c2063f03a9336d
blxerdout <blxerdout@gmail.com>
2023-04-07 07:45:30 -0500

    Corrected File Path

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'));
+  res.sendFile(path.join(__dirname, '/Users/blxerdout/html-site-ec2/creativespxce_site/index.html'));
 });
 
 app.get('/products', (req, res) => {
